628de654e3a80d08418abcd1da6834a06540e91d
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-07-06 14:10:14 -0700

    Delete button working; fully functional application

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -43,14 +43,14 @@ $(function() {
       );
     });
   
-    $(".delete-burger").on("click", event => {
+    $(".delete-burger").on("click", function(event) {
       const id = $(this).data("id");
   
       // Send the DELETE request.
       $.ajax("/api/burgers/" + id, {
         type: "DELETE"
       }).then(
-        () => {
+        function() {
           console.log("deleted burger", id);
           // Reload the page to get the updated list
           location.reload();
