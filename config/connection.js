439fac146a42a32e81080a21881e309ed17dd4b1
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-07-06 16:50:48 -0700

    Added JAWSDB_URL environmental variable

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 // Set up MySQL connection
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "burgers_db"
-});
+// Checking to see if server contains JAWSDB_URL environmental variable. If not, local database is used
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "burgers_db"
+  });
+};
 
 // Make connection
 connection.connect(err => {
